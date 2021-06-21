71bb3971659702fcb4af7ddc1cd2dbd1b2639c1d
Esteban <estebanarivasv@gmail.com>
2021-06-21 13:01:58 -0300

    Refactor URI variables

--- main/config/flask_config.py
+++ main/config/flask_config.py
@@ -1,9 +1,16 @@
 import os
 
+DB_TYPE = os.getenv('DATABASE_TYPE')
+DB_USERNAME = os.getenv('MYSQL_USER')
+DB_ROOT_PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD')
+DB_SERVER = os.getenv('DATABASE_SERVER')
+DB_NAME = os.getenv('MYSQL_DATABASE')
+
 
 class Config(object):
     """Base config, uses staging database server."""
-    SQLALCHEMY_DATABASE_URI = f"{os.getenv('DATABASE_TYPE')}{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('DATABASE_SERVICE')}/{os.getenv('MYSQL_DATABASE')}"
+    SQLALCHEMY_DATABASE_URI = DB_TYPE + DB_USERNAME + ":" + DB_ROOT_PASSWORD + "@" + DB_SERVER + "/" + DB_NAME
+    # TODO: DELETE mysql://username:password@server/db
 
 
 # This classes heredate from Config
