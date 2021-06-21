35b5a5a3ba2d97552468e44a0e290ce97ca5a9c0
Giancarlo <g.galvarini@alumno.um.edu.ar>
2021-06-21 15:49:57 -0300

    fix

---
+++ main/services/mail_service.py
@@ -0,0 +1,22 @@
+from threading import Thread
+
+from flask import render_template
+from flask_mail import Mail
+
+from app import app
+
+
+def sendMail(to, subject, template, **kwargs):
+    msg = Mail.Message(subject, sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template('mail/' + template + '.txt', **kwargs)
+    msg.html = render_template('mail/' + template + '.html', **kwargs)
+    thr = Thread(target=mail_sender, args=[app, msg])
+    thr.start()  # Iniciar hilo
+
+
+def mail_sender(app, msg):
+    with app.app_context():
+        Mail.send(msg)  # send es la que envia el mail
+
+# se que esto no va a aca, pero asi seria el envio de un mensaje
+# sendMail('correo@ejemplo.com', 'mensaje', 'nombre del template')
