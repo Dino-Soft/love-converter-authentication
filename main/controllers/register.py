e90eb49392949bce9974c1455aa1b8a420fb4466
gianca1994 <gianca9405@gmail.com>
2021-08-09 00:06:52 -0300

    rm lib64 dir

--- main/controllers/register.py
+++ main/controllers/register.py
@@ -1,10 +1,11 @@
+from re import I
 from flask import request
 from flask_restful import Resource
 from main.extensions import db
 from main.mappers import UserMapper
 from main.models import UserModel
 from main.validators import *
-
+from werkzeug.security import generate_password_hash
 
 user_mapper = UserMapper()
 
@@ -17,20 +18,21 @@ class Register(Resource):
         
         # TODO Deshabilitado porque trae problemas -->> Corregir.
         
-        # if check_email(json["email"]):
-        email_validator(json["email"])
-        
-        # TODO aca se deberia encriptar la password...
-        #encrypt_password = ....plain_password(json.get("password"))
-
-
         if json != "":
             # TODO Deshabilitado porque trae problemas -->> Corregir.
             #if check_user(json["username"]) and check_password(json["password"]):
+            
+             # if check_email(json["email"]):
+            email_validator(json["email"])
+            
+            # TODO aca se deberia encriptar la password...
+            # user_repository.plain_password(json.get("password"))
+
+            password_encrypted = generate_password_hash(json.get("password"))
 
             user_instance = UserModel(
                 email=json.get("email"),
-                password=json.get("password"),
+                password=password_encrypted,
                 username=json.get("username"),
                 deleted=False,
                 activated=False,
--- main/models/user.py
+++ main/models/user.py
@@ -5,12 +5,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, index=True, nullable=False)
     email = db.Column(db.String(100), unique=True, index=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     deleted = db.Column(db.Boolean, nullable=False)
     activated = db.Column(db.Boolean, nullable=False)
     last_updated = db.Column(db.DateTime, nullable=False)
     last_access = db.Column(db.DateTime, nullable=False)
-
+    
     # User object representation
     def __repr__(self):
         return f'<User: {self.id_num} {self.email} >'
