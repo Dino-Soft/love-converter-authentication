34af4760f5a4143467d94adcd74fefc1fe49941e
Esteban <estebanarivasv@gmail.com>
2021-08-13 02:41:39 -0300

    Initial register fix

--- main/services/user.py
+++ main/services/user.py
@@ -1,7 +1,6 @@
-from werkzeug.security import check_password_hash, generate_password_hash
-
-from main.extensions import db
 from main.repositories import UserRepository
+from main.models import UserModel
+from main.validators import email_validator
 
 repository = UserRepository()
 
@@ -12,31 +11,32 @@ class User:
         Solamente ejecutan y devuelven algo.
     """
 
-    # Password security management
-    @staticmethod
-    def plain_password():
-        repository.plain_password()
-
-    @staticmethod
-    def plain_password(password):
-        repository.plain_password(password)
-
-    @staticmethod
-    def validate_password(password):
-        repository.validate_password(password)
-
     @staticmethod
     def get_user_by_id(id):
-        repository.get_register_by_id(id)
+        repository.get_user_by_id(id)
 
     @staticmethod
     def delete_user_by_id(id):
-        repository.delete_register_by_id(id)
+        repository.delete_user_by_id(id)
 
     @staticmethod
     def edit_user_by_id(id, data):
-        repository.edit_register_by_id(id, data)
+        repository.edit_user_by_id(id, data)
 
     @staticmethod
-    def add_user(email, data):
-        repository.create_register(email, data)
+    def add_user(data):
+        # Verify if the email exists
+        email_validator(data["email"])
+
+        try:
+            # Generate user instance
+            user_instance = UserModel(
+                email=data["email"],
+                plain_password=data["password"],
+                username=data["username"]
+            )
+            repository.create_user(user_instance)
+        except Exception as error:
+            return error, 409
+
+
