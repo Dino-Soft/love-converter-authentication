a9389f61986961d9a1506812d2d0869a7fd884dd
Esteban <estebanarivasv@gmail.com>
2021-08-13 02:42:00 -0300

    Test autogenerated data in db

--- main/models/user.py
+++ main/models/user.py
@@ -1,3 +1,6 @@
+from sqlalchemy import sql
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from main.extensions import db
 
 
@@ -6,11 +9,26 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, index=True, nullable=False)
     email = db.Column(db.String(100), unique=True, index=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    deleted = db.Column(db.Boolean, nullable=False)
-    activated = db.Column(db.Boolean, nullable=False)
-    last_updated = db.Column(db.DateTime, nullable=False)
-    last_access = db.Column(db.DateTime, nullable=False)
-    
+    deleted = db.Column(db.Boolean, server_default=False)
+    activated = db.Column(db.Boolean, server_default=False)
+    last_updated = db.Column(db.DateTime, server_default=sql.func.now())
+    last_access = db.Column(db.DateTime, server_default=sql.func.now())
+
+    # Password security management
+    @property
+    def plain_password(self):
+        raise AttributeError("The password can not be obtained. It is prohibited.")
+        # We won't obtain the password accessing with a get method
+
+    @plain_password.setter
+    def plain_password(self, password):
+        self.password = generate_password_hash(password)
+        # We encrypt the plain text password from the JSON received in the user registration
+
+    def validate_password(self, password):
+        return check_password_hash(self.password, password)
+        # Compares the received password with the database password
+
     # User object representation
     def __repr__(self):
         return f'<User: {self.id_num} {self.email} >'
