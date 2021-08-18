ed39e7c8bb9f6d5807e448f10293ce3327ff7690
gianca1994 <gianca9405@gmail.com>
2021-08-17 21:36:51 -0300

    refactor-names-regular-expressions

--- main/validators/user.py
+++ main/validators/user.py
@@ -4,8 +4,8 @@ import re
 from marshmallow import validate
 
 
-check_email = r'^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{1,15}$'
-check_password = r'^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{12,30}$'
+regular_email = r'^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{1,15}$'
+regular_password = r'^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{12,30}$'
 
 
 def get_email_existance(email):
@@ -19,7 +19,7 @@ def get_email_existance(email):
 
 # TODO: HACER TEST PARA "check_email", "check_user" Y "check_password" METODOS..
 def check_email(email):
-    return re.match(check_email, email) is not None if len(email) <= 100 else False
+    return re.match(regular_email, email) is not None if len(email) <= 100 else False
 
 
 def check_user(username):
@@ -27,4 +27,4 @@ def check_user(username):
 
 
 def check_password(password):
-    return re.match(check_password, password) is not None
+    return re.match(regular_password, password) is not None
